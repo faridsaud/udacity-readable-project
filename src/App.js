19f295a9a2e5a3e911804e9917cf637796039a6f
Farid Saud Rolleri <faridsaud@yahoo.com>
2017-10-03 21:04:30 -0500

    fix: Remove unnecessary connect to store in App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Route} from 'react-router-dom';
@@ -7,33 +7,51 @@ import Menu from "./components/Menu";
 import CategoryHomePage from "./components/CategoryHomePage";
 import CreatePost from "./components/CreatePost";
 import PostDetail from "./components/PostDetail";
+import {connect} from 'react-redux'
 
-class App extends Component {
-  render() {
-    return (
-        <div className="app">
-            <Menu/>
-            <br/>
-            <Route exact path='/' render={() => (
-                <HomePage />
-            )}/>
-            <Route exact path='/category/:id' render={(props) => (
-                <CategoryHomePage />
-            )}/>
-            <Route exact path='/post/create' render={(props) => (
-                <CreatePost />
-            )}/>
-            <Route exact path='/post/update/:id' render={(props) => (
-                <CreatePost isUpdate={true}/>
-            )}/>
-            <Route exact path='/post/detail/:id' render={(props) => (
-                <PostDetail />
-            )}/>
-        </div>
+export default class App extends Component {
+
+    componentDidMount() {
+        console.log("Props APP:", this.props);
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <Menu/>
+                <br/>
+                <Route exact path='/' render={() => (
+                    <HomePage />
+                )}/>
+                <Route exact path='/category/:id' render={(props) => (
+                    <CategoryHomePage />
+                )}/>
+                <Route exact path='/post/create' render={(props) => (
+                    <CreatePost />
+                )}/>
+                <Route exact path='/post/update/:id' render={(props) => (
+                    <CreatePost isUpdate={true}/>
+                )}/>
+                <Route exact path='/post/detail/:id' render={(props) => (
+                    <PostDetail />
+                )}/>
+            </div>
 
 
-    );
-  }
+        );
+    }
 }
+/*
+ const mapStateToProps = (state, props)=>({
+ categories: state.categories,
+ comments: state.comments,
+ posts:state.posts
 
-export default App;
+ });
+ */
+
+const mapStateToProps = (state, props) => ({
+    categories: state.categories
+
+});
+
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,9 +3,10 @@
  */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
 
 
-class Comment extends Component {
+export class Comment extends Component {
 
     state = {
         isEditable: false
@@ -17,7 +18,9 @@ class Comment extends Component {
         }))
     };
 
-
+    componentDidMount() {
+        console.log("Props COMMENT:", this.props);
+    }
 
     render() {
         let text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
@@ -58,4 +61,13 @@ class Comment extends Component {
 }
 
 Comment.propTypes = {};
-export default Comment;
+
+const mapStateToProps = (state, props)=>({
+    comments: state.comments,
+    posts:state.posts
+
+});
+
+export default connect(
+    mapStateToProps
+)(Comment)
